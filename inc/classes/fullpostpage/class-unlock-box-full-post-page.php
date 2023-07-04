be7a83a248e641b992f80c3e2a91c73c65fd4891
施荣 <1309873659@qq.com>
2023-07-04 10:23:27 +0800

    fix post lock

--- inc/classes/fullpostpage/class-unlock-box-full-post-page.php
+++ inc/classes/fullpostpage/class-unlock-box-full-post-page.php
@@ -41,7 +41,7 @@ class Unlock_Box_Full_Post_Page {
 		/**
 		 * Filters.
 		 */
-		add_filter( 'the_content', array( $this, 'trigger_unlockprotocol_flow' ) );	
+		add_filter( 'the_content', array( $this, 'trigger_unlockprotocol_flow' ) );
     }
 	
 	/**
@@ -50,7 +50,7 @@ class Unlock_Box_Full_Post_Page {
 	 * @since 4.0.0
 	 *
 	 * @param string $content The original post content.
-	 * 
+	 *
 	 */
 	public function trigger_unlockprotocol_flow( $content ) {
 		// Get the current post ID.
@@ -58,12 +58,12 @@ class Unlock_Box_Full_Post_Page {
 
 		// Retrieve the saved attributes.
 		$attributes = get_post_meta( $post_id, 'unlock_protocol_post_locks', true );
-
-		if ( ! empty( $attributes ) ) {
-			$locks = json_decode($attributes, true);
+		
+        $locks = json_decode($attributes, true);
+		if ( ! empty( $locks ) ) {
 			// Call render_content() to trigger the Unlock Protocol flow.
 			return Unlock::render_content( $locks, $content );
-		} else {			
+		} else {
 			return $content;
 		}
 	}
